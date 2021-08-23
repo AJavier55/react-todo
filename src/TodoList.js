d8cf08c16a101850c24611be32ee3c22ea6f711b
AJavier55 <aliciajavier5@gmail.com>
2021-08-23 18:21:33 -0400

    todo component started

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,1 +1,14 @@
 import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+          return;
+        }
+
+        const newTodos = [todo, .React.React.todos]
+}
